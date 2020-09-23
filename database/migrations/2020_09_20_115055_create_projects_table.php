9348006539047c3e48e7f9c0bae86d22fc10bc2f
themepress360 <themepress360@gmail.com>
2020-09-24 02:27:42 +0500

    code updated

--- database/migrations/2020_09_20_115055_create_projects_table.php
+++ database/migrations/2020_09_20_115055_create_projects_table.php
@@ -15,7 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('project_title');
+            $table->text('project_title');
             $table->string('description');
             $table->bigInteger('clients');
             $table->bigInteger('department');
